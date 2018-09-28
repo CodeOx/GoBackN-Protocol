bcb2bedfa963a98f116dbf6e27c551be59d98990
shashwat <shashwatbanchhor12@gmail.com>
2018-09-28 16:34:43 +0530

    Data Link Events working for string

--- src/go_back_n.py
+++ src/go_back_n.py
@@ -1,27 +1,39 @@
+import time
+import optparse
+import threading
+import datetime
+import physicalLayer
+from physicalLayer import Frame,Packet
+
 MAX_SEQ = 7
+PACKET_READY = False
+EVENT = 0
+BUFFER =[]
+
+parser = optparse.OptionParser()
+parser.add_option('-s', dest='src_ip', default='')
+parser.add_option('-d', dest='dest_ip', default='')
+parser.add_option('-m', dest='method', default='recieve')
+parser.add_option('-p', dest='port', type='int', default=12345)
+(options, args) = parser.parse_args()
+src_ip = options.src_ip
+dest_ip = options.dest_ip
+port = options.port
+method = options.method
+
+
+
+
 
 def between(a,b,c):
-	if (((a<=b)&&(b<c)) || ((c<a) && (a<=b)) || ((b<c)&&(c<a))):
+	if (((a<=b)and(b<c)) or ((c<a) and (a<=b)) or ((b<c)and(c<a))):
 		return True
-	else
+	else:
 		return False
-
-class Frame(object):
-	"""docstring for Frame"""
-	def __init__(self, information,sequence,acknowledgement):
-		super(Frame, self).__init__()
-		self.info = information
-		self.seq = sequence
-		self.ack = acknowledgement
-
-class Packet(object):
-	"""docstring for Packet"""
-	def __init__(self, arg):
-		super(Packet, self).__init__()
-		self.arg = arg
 		
 def send_data(frame_nr,frame_expected,buffer):
 	s = Frame(buffer[frame_nr],frame_nr,(frame_expected+MAX_SEQ)%(MAX_SEQ+1))
+	physicalLayer.sendPacket(buffer[frame_nr],dest_ip, port)
 	# send to physical layer
 	# start time
 
@@ -35,57 +47,83 @@ class Seq_nr(object):
 	def inc(self):
 		self.arg = (self.arg+1)%(MAX_SEQ+1)
 		
+def data_link_enable():
+	global EVENT
+	t0 = time.time()	
+	next_frame_to_send = (-1)
+	ack_expected = (-1)
+	frame_expected = (-1)
+	r  = Frame(None,None,None)
+	#packet_outbound = buffer[MAX_SEQ+1]
+	nbuffered = (-1)
 
-next_frame_to_send = (-1)
-ack_expected = (-1)
-frame_expected = (-1)
-r  = Frame(None,None,None)
-packet_outbound = buffer[MAX_SEQ+1]
-nbuffered = (-1)
-
-enable_network_layer()
-ack_expected = 0
-frame_expected = 0
-next_frame_to_send = 0
-nbuffered  =0 
-
-
-
-
-
-while True:
-
-	if event < 4:
-		if event == 0: # frame arrival
-			r = get_from_phy_layer()
-			if r.seq == frame_expected:
-				to_network_layer(r.info)
-				frame_expected += 1
-
-			while (between(ack_expected,r.ack,next_frame_to_send)):
-				nbuffered = nbuffered -1
-				stop_timer()
-				ack_expected+=1
+	#enable_network_layer()
+	ack_expected = 0
+	frame_expected = 0
+	next_frame_to_send = 0
+	nbuffered  =0 
 
 
-		elif event==1: # cksum_err
-			pass
-		elif event==2: # timeout
-			next_frame_to_send = ack_expected
-			for i in xrange(1,nbuffered+1):
-				send_data(next_frame_to_send,frame_expected,buffer)
-				next_frame_to_send+= 1
-
-		elif event ==3: # network_layer_ready
-			# from net_layer
-			new_packet  = buffer[next_frame_to_send]
-			nbuffered = nbuffered +1
-			send_data(next_frame_to_send,frame_expected,buffer)
-			next_frame_to_send += 1 
 
 
-	if nbuffered<MAX_SEQ :
-		# enable net lay
-	else
-		# disable 
-		
+
+	while (time.time()-t0 < 50):
+
+		if EVENT < 4:
+			if physicalLayer.physical_layer_ready(): # frame arrival
+				r = physicalLayer.from_physical_layer()
+				f = open('abc.txt', 'w')
+				f.write(r)
+				f.flush()
+				#if r.seq == frame_expected:
+					#write to file
+					#to_network_layer(r.info)
+					#frame_expected += 1
+
+				#while (between(ack_expected,r.ack,next_frame_to_send)):
+				#	nbuffered = nbuffered -1
+					#stop_timer()
+				#	ack_expected+=1
+
+
+			elif EVENT==1: # cksum_err
+				pass
+			elif EVENT==2: # timeout
+				next_frame_to_send = ack_expected
+				for i in xrange(1,nbuffered+1):
+					send_data(next_frame_to_send,frame_expected,buffer)
+					next_frame_to_send+= 1
+
+			elif EVENT ==3: # network_layer_ready
+				# from net_layer
+				#new_packet = 'abcd'
+				new_packet = Packet('abcd')				
+				BUFFER.append(new_packet)
+				#new_packet  = buffer[next_frame_to_send]
+				nbuffered = nbuffered +1
+				send_data(next_frame_to_send,frame_expected,BUFFER)
+				next_frame_to_send += 1 
+				EVENT = 0
+
+
+		if nbuffered<MAX_SEQ :
+			print
+			# enable net lay
+		else:
+			print
+			# disable 
+
+
+physicalLayer.start_physical_layer(src_ip, port)
+t1 = threading.Thread(target= physicalLayer.recievePacket, name='t1')
+t2 = threading.Thread(target = data_link_enable, name='t2')
+
+if method=='send' : 	
+	EVENT = 3
+
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+
+			
--- src/physicalLayer.py
+++ src/physicalLayer.py
@@ -1,44 +1,90 @@
 import socket
 from scapy.all import *
-import optparse
 import time
 import datetime
 
-parser = optparse.OptionParser()
-parser.add_option('-s', dest='src_ip', default='')
-parser.add_option('-d', dest='dest_ip', default='')
-parser.add_option('-m', dest='method', default='recieve')
-parser.add_option('-p', dest='port', type='int', default=12345)
-(options, args) = parser.parse_args()
-src_ip = options.src_ip
-dest_ip = options.dest_ip
-port = options.port
-method = options.method
+class Frame(object):
+	"""docstring for Frame"""
+	def __init__(self, information,sequence,acknowledgement):
+		super(Frame, self).__init__()
+		self.info = information
+		self.seq = sequence
+		self.ack = acknowledgement
+
+class Packet(object):
+	"""docstring for Packet"""
+	def __init__(self, arg):
+		super(Packet, self).__init__()
+		self.arg = arg
+
+
+packet_recieved = False
+message = ''
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind((src_ip, port))
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+def start_physical_layer(src_ip, port):
+    server_socket.bind((src_ip, port))
+
+def physical_layer_ready():
+    global packet_recieved
+    if packet_recieved :
+	packet_recieved = False
+	return True
+    return False
+
+def from_physical_layer():
+    global message
+    return message
+
 def recievePacket():
+    global packet_recieved, message
     message = ''
+    f2 = open('test1.txt', 'w')
+    f2.write(str(datetime.datetime.now()))
+    f2.flush()
     while True :
 	    message, address = server_socket.recvfrom(512)
 	    #return Ether(message)
 	    f = open('main.txt','w')
 	    f.write(message)
 	    f.flush()
-    return message
+	    packet_recieved = True
+	    return
+    return
 
 def sendPacket(packet, ip, port):
-    message = str(packet)
+    f3 = open('test2.txt', 'w')
+    f3.write(str(packet.arg))
+    f3.flush()
+    message = str(packet.arg)
     addr = (ip, port)
     client_socket.sendto(message, addr)
 
+def write():
+    fi = open('random.txt', 'w')
+    for i in range(10) :
+        fi.write(str(datetime.datetime.now()) + "\n")
+	fi.flush()
+	time.sleep(1)    
+
+    
+
 def main():
-	if(method == "recieve") :
-		recievePacket()
-	else :
-		sendPacket('Shashwat Banchhor' , dest_ip, port)
+	print
+	#go_back_n.data_link_enable()
+	#if(method == "recieve") :
+	#	t1 = threading.Thread(target=recievePacket, name='t1')
+	#	t2 = threading.Thread(target=write, name='t2')
+        #        t1.start()
+	#	t2.start()
+	#	t1.join()
+	#	t2.join()
+		#recievePacket()
+	#else :
+	#	time.sleep(5)
+	#	sendPacket(str(datetime.datetime.now()) , dest_ip, port)
 
 if __name__ == "__main__":
     main()
